ebc8345659f411df4e159148bd36bbb69b7b0e6e
LauraCaballero <lauracaballer19@gmail.com>
2026-03-24 10:46:51 +0100

    feat: update project structure and add new pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
 const name = 'world';
 console.log(`Hello ${name}`);
+
+
+
+
+// DETAILS
+
+const details = document.querySelectorAll("details");
+
+details.forEach((detail) => {
+  const summary = detail.querySelector("summary");
+
+  detail.addEventListener("toggle", () => {
+    if (detail.open) {
+      summary.textContent = "Read less";
+    } else {
+      summary.textContent = "Read more";
+    }
+  });
+});
